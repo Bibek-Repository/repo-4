6d6abfcea932bd21908f46f13bfdbb896418afcf
bibekster <megadextron911@gmail.com>
2024-10-30 10:41:41 +0545

    Constants in SciPY

---
+++ SciPy_python/readme.md
@@ -0,0 +1,4 @@
+Scipy 
+It stands for Scientific python. It is a scientific computation library that uses NumPy. It provides more utility functions for optimization, statistics and signal processing. It is open source. It was created by Travis Olliphant.
+SciPy has optimized and added functions that are frequently used in NumPy and Data Science. SciPy is written in python and some few segments in C. The github repository for SciPy is located at: https://github.com/scipy/scipy 
+ 
---
+++ SciPy_python/sourcecode.py
@@ -0,0 +1,154 @@
+from scipy import constants
+import scipy
+
+print(constants.liter)   # This will return 1 liter as cubic meters.
+
+# To check SciPy Version
+print(scipy.__version__)
+
+# To print the constant value of PI:
+print(constants.pi)
+
+# Constant Units
+print(dir(constants))
+
+# Metric (SI) Prefixes:
+print(constants.centi)
+print(constants.yotta)
+print(constants.exa)
+print(constants.peta)
+print(constants.tera)
+print(constants.giga)
+print(constants.mega)
+print(constants.kilo)
+print(constants.hecto)
+print(constants.deka)
+print(constants.deci)
+print(constants.centi)
+print(constants.milli)
+print(constants.micro)
+print(constants.nano)
+print(constants.pico)
+print(constants.femto)
+print(constants.atto)
+print(constants.zepto)
+
+# Binary Prefixes:
+print(constants.kibi)
+print(constants.mebi)
+print(constants.gibi)
+print(constants.tebi)
+print(constants.pebi)
+print(constants.exbi)
+print(constants.zebi)
+print(constants.yobi)
+
+# Mass
+print(constants.gram)
+print(constants.metric_ton)
+print(constants.grain)
+print(constants.lb)
+print(constants.pound)
+print(constants.oz)
+print(constants.ounce)
+print(constants.stone)
+print(constants.long_ton)
+print(constants.short_ton)
+print(constants.troy_ounce)
+print(constants.troy_pound)
+print(constants.carat)
+print(constants.atomic_mass)
+print(constants.m_u)
+print(constants.u)
+
+# Angle
+print(constants.degree)
+print(constants.arcmin)
+print(constants.arcminute)
+print(constants.arcsec)
+print(constants.arcsecond)
+
+# Time
+print(constants.minute)
+print(constants.hour)
+print(constants.day)
+print(constants.week)
+print(constants.year)
+print(constants.Julian_year)
+
+# Length
+print(constants.inch)
+print(constants.foot)
+print(constants.yard)
+print(constants.mile)
+print(constants.mil)
+print(constants.pt)
+print(constants.point)
+print(constants.survey_foot)
+print(constants.survey_mile)
+print(constants.nautical_mile)
+print(constants.fermi)
+print(constants.angstrom)
+print(constants.micron)
+print(constants.au)
+print(constants.astronomical_unit)
+print(constants.light_year)
+print(constants.parsec)
+
+# Pressure
+print(constants.atm)
+print(constants.atmosphere)
+print(constants.bar)
+print(constants.torr)
+print(constants.mmHg)
+print(constants.psi)
+
+# Area
+print(constants.hectare)
+print(constants.acre)
+
+# Volume
+print(constants.liter)
+print(constants.litre)
+print(constants.gallon)
+print(constants.gallon_US)
+print(constants.gallon_imp)
+print(constants.fluid_ounce)
+print(constants.fluid_ounce_US)
+print(constants.barrel)
+print(constants.bbl)
+
+# Speed:
+print(constants.kmh)
+print(constants.mph)
+print(constants.mach)
+print(constants.speed_of_sound)
+print(constants.knot)
+
+# Temperature
+print(constants.zero_Celsius)
+print(constants.degree_Fahrenheit)
+
+# Energy
+print(constants.ev)
+print(constants.electron_volt)
+print(constants.calorie)
+print(constants.calorie_th)
+print(constants.calories_IT)
+print(constants.erg)
+print(constants.Btu)
+print(constants.Btu_IT)
+print(constants.Btu_th)
+print(constants.ton_TNT)
+
+# Power
+print(constants.hp)
+print(constants.horsepower)
+
+# Force
+print(constants.dyn)
+print(constants.dyne)
+print(constants.lbf)
+print(constants.pound_force)
+print(constants.kgf)
+print(constants.kilogram_force)
